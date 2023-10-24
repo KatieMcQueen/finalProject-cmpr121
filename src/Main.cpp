a4e9ef1808edf6a869591b8120cf2f71a8a7908d
Katelyn McQueen <54079638+KatieMcQueen@users.noreply.github.com>
2023-10-24 16:00:24 -0700

    created some placehodler files

---
+++ src/Main.cpp
@@ -0,0 +1,6 @@
+//tells the testing library to implement a main function to serve as a test driver
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+//include the testing library header
+#include "doctest.h"
+
+
